aaa268a0fb7fc55a1a0fc497d6e7e2358e02a245
Bishnu <thapabishnu20@gmail.com>
2023-10-18 21:43:05 +0545

    background color changer task done

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,27 @@ function App() {
 
   return (
     <>
-      <div className="bg-green-500 w-100 h-80"></div>
+      <div
+        className="bg-green-500 w-100 h-80"
+        style={{ backgroundColor: color }}
+      ></div>
       <div className="my-5 grid justify-items-center w-100">
-        <button className="bg-red-600 rounded mx-auto px-5 py-2 text-white">
+        <button
+          onClick={() => setColor("red")}
+          className="bg-red-600 justify-center rounded mb-4 px-5 py-2 text-white"
+        >
           Red
         </button>
-        <button className="bg-red-600 rounded mx-auto px-5 py-2 text-white">
-          Black
+        <button
+          onClick={() => setColor("yellow")}
+          className="bg-yellow-500 rounded mb-4 px-5 py-2 text-white"
+        >
+          Yellow
         </button>
-        <button className="bg-red-600 rounded mx-auto px-5 py-2 text-white">
+        <button
+          onClick={() => setColor("blue")}
+          className="bg-blue-600 rounded mb-4 px-5 py-2 text-white"
+        >
           Blue
         </button>
       </div>
